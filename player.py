c14019897c662be9a6dd793d8a8d6271986535e3
David <gomezd6@mcmaster.ca>
2024-03-08 11:15:45 -0500

    Updated instance variables & constructor

--- player.py
+++ player.py
@@ -1,7 +1,4 @@
 class Player:
-    def __init__(self, username, isHuman, finished, points, date):
+    def __init__(self, username, points, date):
         self.username = username
-        self.isHuman = isHuman
-        self.finished = finished
         self.points = points
-        self.date = date
