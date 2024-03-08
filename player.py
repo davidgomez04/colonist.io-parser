2749af77de9ed37b0a48937ebba9730d2f3a5a86
David <gomezd6@mcmaster.ca>
2024-03-08 13:19:46 -0500

    Updated player class with total stats

--- player.py
+++ player.py
@@ -1,4 +1,6 @@
 class Player:
-    def __init__(self, username, points, date):
+    def __init__(self, username, total_wins, games_played, total_points):
         self.username = username
-        self.points = points
+        self.total_wins = total_wins
+        self.games_played = games_played
+        self.total_points = total_points
