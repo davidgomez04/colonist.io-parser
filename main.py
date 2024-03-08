9d6c6642cb4c91a9beab9762ad7d4040c1a504f8
David <gomezd6@mcmaster.ca>
2024-03-08 11:22:55 -0500

    Added functionality to parse multiple users

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 import requests
 from datetime import datetime
-url = 'https://colonist.io/api/profile/elninokr/history'
 
-playerToParse = 'elninokr'
-
-response = requests.get(url)
-
-if response.status_code == 200:
-    json_data = response.json()
-else:
-    print(f"Error: Failed to fetch data from {url}. Status code: {response.status_code}")
+playerToParse = ['elninokr', 'rexy9880']
 
 # def get_start_date(start_date):
 #     year = start_date.year
@@ -17,15 +9,23 @@ else:
 #     day = start_date.day
 #     return year + "-" + month + "-" + day
 
-gamesPlayed = 0
-totalPoints = 0
-for game in json_data:
-    start_time_ms = int(game["startTime"])
-    start_time_seconds = start_time_ms / 1000
-    start_date = datetime.fromtimestamp(start_time_seconds)
-    # get_start_date(start_date)
-    for player in game["players"]:
-        if playerToParse == player["username"] and player["finished"]:
-            print("\tPoints:", player["points"])
-            totalPoints += player["points"]
-            gamesPlayed+=1
+for p in playerToParse:
+    gamesPlayed = 0
+    totalPoints = 0
+    url = 'https://colonist.io/api/profile/{}/history'.format(p)
+    response = requests.get(url)
+    if response.status_code == 200:
+        json_data = response.json()
+    else:
+        print(f"Error: Failed to fetch data from {url}. Status code: {response.status_code}")
+
+    for game in json_data:
+        start_time_ms = int(game["startTime"])
+        start_time_seconds = start_time_ms / 1000
+        start_date = datetime.fromtimestamp(start_time_seconds)
+        # get_start_date(start_date)
+        for player in game["players"]:
+            if p == player["username"] and player["finished"]:
+                totalPoints += player["points"]
+                gamesPlayed+=1
+    print(p + " total points " + str(totalPoints))
