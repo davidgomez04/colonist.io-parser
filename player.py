6a3d430c47577d331778259c1c1e09ef9bace6e8
David <gomezd6@mcmaster.ca>
2024-03-08 10:45:26 -0500

    Added player class

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, username, isHuman, finished, points, date):
+        self.username = username
+        self.isHuman = isHuman
+        self.finished = finished
+        self.points = points
+        self.date = date
